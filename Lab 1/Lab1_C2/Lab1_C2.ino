9e383ca801d2e0565302e867aaee21e4b8409bfd
hghg005 <hghg005005@gmail.com>
2022-01-17 13:37:13 -0800

    Challnege

---
+++ Lab 1/Lab1_C2/Lab1_C2.ino
@@ -0,0 +1,21 @@
+const int time=0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//When the add time button is pressed, for every 100ms the add time button is pressed, 
+//1 second is added to the timer. Timer should not count down while the add time button is pressed.
+
+Pressing the reset button will reset the timer to 0 and not buzz the motor. 
+
+void resetButton()
+{
+    
+}
